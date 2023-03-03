f9cbe17a967db13b98f0dcfc224de5296434b360
bbqbop <dierk.peters@gmail.com>
2023-03-03 18:44:11 -0500

    limit the loop-iterations to the number input

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,9 +1,9 @@
 const fibonacci = function(num) {
 
 if (num < 0) return 'OOPS';
-    
+
 let fibonacciSeq = [1,1,2];
-    for (i = 3; i < 30 ; i++){
+    for (i = 3; i < num ; i++){
         fibonacciSeq.push(fibonacciSeq[i-1]+fibonacciSeq[i-2])
     }
 return fibonacciSeq[num-1];
