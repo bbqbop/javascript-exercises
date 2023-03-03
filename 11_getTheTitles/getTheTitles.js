2aba117049dc8251c48398d382dd4032d8d57e71
bbqbop <dierk.peters@gmail.com>
2023-03-03 18:52:08 -0500

    solve ex 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookArray = []
+    books.forEach(book => {
+        bookArray.push(book.title)
+    })
+    return bookArray;
 };
 
 // Do not edit below this line
