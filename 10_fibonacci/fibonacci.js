c80882c58433b4e60beec5490bbebdcdc104f8ee
bbqbop <dierk.peters@gmail.com>
2023-03-03 18:41:53 -0500

    solve ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+if (num < 0) return 'OOPS';
+    
+let fibonacciSeq = [1,1,2];
+    for (i = 3; i < 30 ; i++){
+        fibonacciSeq.push(fibonacciSeq[i-1]+fibonacciSeq[i-2])
+    }
+return fibonacciSeq[num-1];
 
 };
 
