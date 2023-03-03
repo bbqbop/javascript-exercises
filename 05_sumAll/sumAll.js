f8b2f7227426d64e9e03d12bcee91d8ff00ed2f0
bbqbop <dierk.peters@gmail.com>
2023-03-03 15:59:40 -0500

    solve ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x <= 0 || y <= 0 || typeof(x && y) != 'number') return 'ERROR';
+    let iter = y > x ? x : y;
+    let limit = y > x ? y : x;
+    sum = 0;
+    for (iter; iter<= limit; iter++){
+        sum += iter; 
+    }
+
+    return sum;
 
 };
 
