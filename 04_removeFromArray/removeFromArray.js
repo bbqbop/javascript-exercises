0a0427b24b886d38b5f850b17e419e87bee059d9
bbqbop <dierk.peters@gmail.com>
2023-03-03 14:47:41 -0500

    solve ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+   
+    for (i of args){
+        let index = array.indexOf(i);
+        if (index === -1) continue;
+        array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
