bc580c55af1a94a8ac0c3f10989aa1d7c8c97d3b
bbqbop <dierk.peters@gmail.com>
2023-03-03 17:37:37 -0500

    solve ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,47 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(array) {
+	let sum = 0;
+  for (num of array){
+    sum += num;
+  }
+  return sum;
+};
+
+const multiply = function(array) {
+  let total = array[0];
+  for (num of array.splice(1)){
+    total *= num;
+  }
+  return total;
+};
+
+const power = function(a, b) {
+  let numPow = a;
+	for (i = 1; i < b; i++){
+    numPow *= a;
+  }
+  return numPow;
+};
+
+const factorial = function(num) {
+  if (num === 0 || num === 1) return 1;
+  else {
+    for (let i = num - 1; i >= 1; i--){
+      num *= i;
+    }
+    return num;
+  }
 	
 };
 
-const subtract = function() {
-	
-};
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
