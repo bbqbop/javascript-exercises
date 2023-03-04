5cfd831b2e583375ffea17bfd9fa92645ea2a740
bbqbop <dierk.peters@gmail.com>
2023-03-03 20:33:55 -0500

    solve ex 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,28 @@
-const caesar = function() {
+const caesar = function(str, shift) {
+    const abc = 'abcdefghijklmnopqrstuvwxyz'
+    let newString = ''
 
+    for (i = 0 ; i < str.length ; i++){
+        let ltr = str[i].toLowerCase()
+        let idx = abc.indexOf(ltr)
+        if (idx == -1) {
+            newString += str[i]; 
+            continue;
+        }
+
+        let newIdx = idx + shift;
+        while (newIdx > 25) newIdx = newIdx - 26;
+        while (newIdx < 0) newIdx = abc.length + newIdx;
+        if (str[i] === str[i].toUpperCase()){
+            newString += abc[newIdx].toUpperCase()
+            continue;
+        }
+        newString += abc[newIdx];
+        
+
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
