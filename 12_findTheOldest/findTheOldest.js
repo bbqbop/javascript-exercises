d3df2113537f6a3fefa1778220b10851671e8d61
bbqbop <dierk.peters@gmail.com>
2023-03-03 19:28:25 -0500

    solve ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+
+return array.reduce((max,val) => {
+    if (max.yearOfDeath === undefined) {
+        max.yearOfDeath = (new Date).getFullYear();
+    }
+    if (val.yearOfDeath - val.yearOfBirth > max.yearOfDeath - max.yearOfBirth) return val;
+    return max;
+})
 
 };
 
